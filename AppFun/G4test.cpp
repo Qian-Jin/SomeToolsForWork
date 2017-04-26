9b2c62834c01c8408a386eba6cf3142a3bc69ba5
Zhukov <1993a822@163.com>
2017-04-26 19:25:16 +0800

    更改G4实现方法。

--- AppFun/G4test.cpp
+++ AppFun/G4test.cpp
@@ -15,62 +15,239 @@ void g4testFileOutput()
 }
 
 
-void makeG4testFile(std::ofstream &file, std::ofstream &judgefile)
-{
-	std::string gcode[17],theG4code,gcode2[17];
 
-	gcode[0] = "N0 G0 X10 Y10 Z10 A10 B10 C10 P10 Q10";
-	gcode[1] = "N0 G1 X10 Y10 Z10 A10 B10 C10 P10 Q10";
-	gcode[2] = "N0 G2 X10 Y10 Z10 A10 B10 C10 P10 Q10 I10 J0";
-	gcode[3] = "N0 G2 X10 Y10 Z10 A10 B10 C10 P10 Q10 R10";
-	gcode[4] = "N0 G2 X10 Y10 Z10 A10 B10 C10 P10 Q10 R-10";
-	gcode[5] = "N0 G3 X10 Y10 Z10 A10 B10 C10 P10 Q10 I10 J0";
-	gcode[6] = "N0 G3 X10 Y10 Z10 A10 B10 C10 P10 Q10 R10";
-	gcode[7] = "N0 G3 X10 Y10 Z10 A10 B10 C10 P10 Q10 R-10";
-	gcode[8] = "N0 G4 K10";
-	gcode[9] = "N0 G17";
-	gcode[10] = "N0 G18";
-	gcode[11] = "N0 G19";
-	gcode[12] = "N0 G50";
-	gcode[13] = "N0 G51 D3";
-	gcode[14] = "N0 G52";
-	gcode[15] = "N0 G90";
-	gcode[16] = "N0 G91";
+std::string makeGcode(int which, int linenum, double pos, int &flat, double radium, int &relative) {
+    std::string str;
 
-	gcode2[0] = "N0 G0 X20 Y20 Z20 A20 B20 C20 P20 Q20";
-	gcode2[1] = "N0 G1 X20 Y20 Z20 A20 B20 C20 P20 Q20";
-	gcode2[2] = "N0 G2 X20 Y20 Z20 A20 B20 C20 P20 Q20 I10 J0";
-	gcode2[3] = "N0 G2 X20 Y20 Z20 A20 B20 C20 P20 Q20 R10";
-	gcode2[4] = "N0 G2 X20 Y20 Z20 A20 B20 C20 P20 Q20 R-10";
-	gcode2[5] = "N0 G3 X20 Y20 Z20 A20 B20 C20 P20 Q20 I10 J0";
-	gcode2[6] = "N0 G3 X20 Y20 Z20 A20 B20 C20 P20 Q20 R20";
-	gcode2[7] = "N0 G3 X20 Y20 Z20 A20 B20 C20 P20 Q20 R-10";
-	gcode2[8] = "N0 G4 K10";
-	gcode2[9] = "N0 G17";
-	gcode2[10] = "N0 G18";
-	gcode2[11] = "N0 G19";
-	gcode2[12] = "N0 G50";
-	gcode2[13] = "N0 G51 D3";
-	gcode2[14] = "N0 G52";
-	gcode2[15] = "N0 G90";
-	gcode2[16] = "N0 G91";
+    if (relative == 91)
+    {
+        pos = 10;
+    }
 
-	
+    switch (which) {
+        case 0:
+            str = "N" + std::to_string(linenum) + " G0" + " X" + std::to_string(pos) + " Y" + std::to_string(pos) +
+                  " Z" + std::to_string(pos) + " A" + std::to_string(pos) + " B" + std::to_string(pos) +
+                  " C" + std::to_string(pos) + " P" + std::to_string(pos) + " Q" + std::to_string(pos) + "\n";
+            break;
+        case 1:
+            str = "N" + std::to_string(linenum) + " G1" + " X" + std::to_string(pos) + " Y" + std::to_string(pos) +
+                  " Z" + std::to_string(pos) + " A" + std::to_string(pos) + " B" + std::to_string(pos) +
+                  " C" + std::to_string(pos) + " P" + std::to_string(pos) + " Q" + std::to_string(pos) + "\n";
+            break;
+        case 2:
+            switch (flat) {
+                case 17:
+                    str = "N" + std::to_string(linenum) + " G2" + " X" + std::to_string(pos) + " Y" +
+                          std::to_string(pos) +
+                          " Z" + std::to_string(pos) + " A" + std::to_string(pos) + " B" + std::to_string(pos) + " C" +
+                          std::to_string(pos) + " P" + std::to_string(pos) + " Q" + std::to_string(pos) + " I" +
+                          std::to_string(radium) + " J0" + "\n";
+                    break;
+                case 18:
+                    str = "N" + std::to_string(linenum) + " G2" + " X" + std::to_string(pos) + " Y" +
+                          std::to_string(pos) +
+                          " Z" + std::to_string(pos) + " A" + std::to_string(pos) + " B" + std::to_string(pos) + " C" +
+                          std::to_string(pos) + " P" + std::to_string(pos) + " Q" + std::to_string(pos) + " K" +
+                          std::to_string(radium) + " I0" + "\n";
+                    break;
+                case 19:
+                    str = "N" + std::to_string(linenum) + " G2" + " X" + std::to_string(pos) + " Y" +
+                          std::to_string(pos) +
+                          " Z" + std::to_string(pos) + " A" + std::to_string(pos) + " B" + std::to_string(pos) + " C" +
+                          std::to_string(pos) + " P" + std::to_string(pos) + " Q" + std::to_string(pos) + " J" +
+                          std::to_string(radium) + " K0" + "\n";
+                    break;
+
+            }
+            break;
+        case 3:
+            switch (flat) {
+                case 17:
+                    str = "N" + std::to_string(linenum) + " G2" + " X" + std::to_string(pos) + " Y" +
+                          std::to_string(pos) +
+                          " Z" + std::to_string(pos) + " A" + std::to_string(pos) + " B" + std::to_string(pos) + " C" +
+                          std::to_string(pos) + " P" + std::to_string(pos) + " Q" + std::to_string(pos) + " R" +
+                          std::to_string(radium) + "\n";
+                    break;
+                case 18:
+                    str = "N" + std::to_string(linenum) + " G2" + " X" + std::to_string(pos) + " Y" +
+                          std::to_string(pos) +
+                          " Z" + std::to_string(pos) + " A" + std::to_string(pos) + " B" + std::to_string(pos) + " C" +
+                          std::to_string(pos) + " P" + std::to_string(pos) + " Q" + std::to_string(pos) + " R" +
+                          std::to_string(radium) + "\n";
+                    break;
+                case 19:
+                    str = "N" + std::to_string(linenum) + " G2" + " X" + std::to_string(pos) + " Y" +
+                          std::to_string(pos) +
+                          " Z" + std::to_string(pos) + " A" + std::to_string(pos) + " B" + std::to_string(pos) + " C" +
+                          std::to_string(pos) + " P" + std::to_string(pos) + " Q" + std::to_string(pos) + " R" +
+                          std::to_string(radium) + "\n";
+                    break;
+            }
+            break;
+        case 4:
+            switch (flat) {
+                case 17:
+                    str = "N" + std::to_string(linenum) + " G2" + " X" + std::to_string(pos) + " Y" +
+                          std::to_string(pos) +
+                          " Z" + std::to_string(pos) + " A" + std::to_string(pos) + " B" + std::to_string(pos) + " C" +
+                          std::to_string(pos) + " P" + std::to_string(pos) + " Q" + std::to_string(pos) + " R-" +
+                          std::to_string(radium) + "\n";
+                    break;
+                case 18:
+                    str = "N" + std::to_string(linenum) + " G2" + " X" + std::to_string(pos) + " Y" +
+                          std::to_string(pos) +
+                          " Z" + std::to_string(pos) + " A" + std::to_string(pos) + " B" + std::to_string(pos) + " C" +
+                          std::to_string(pos) + " P" + std::to_string(pos) + " Q" + std::to_string(pos) + " R-" +
+                          std::to_string(radium) + "\n";
+                    break;
+                case 19:
+                    str = "N" + std::to_string(linenum) + " G2" + " X" + std::to_string(pos) + " Y" +
+                          std::to_string(pos) +
+                          " Z" + std::to_string(pos) + " A" + std::to_string(pos) + " B" + std::to_string(pos) + " C" +
+                          std::to_string(pos) + " P" + std::to_string(pos) + " Q" + std::to_string(pos) + " R-" +
+                          std::to_string(radium) + "\n";
+                    break;
+            }
+            break;
+        case 5:
+            switch (flat) {
+                case 17:
+                    str = "N" + std::to_string(linenum) + " G3" + " X" + std::to_string(pos) + " Y" +
+                          std::to_string(pos) +
+                          " Z" + std::to_string(pos) + " A" + std::to_string(pos) + " B" + std::to_string(pos) + " C" +
+                          std::to_string(pos) + " P" + std::to_string(pos) + " Q" + std::to_string(pos) + " I" +
+                          std::to_string(radium) + " J0" + "\n";
+                    break;
+                case 18:
+                    str = "N" + std::to_string(linenum) + " G3" + " X" + std::to_string(pos) + " Y" +
+                          std::to_string(pos) +
+                          " Z" + std::to_string(pos) + " A" + std::to_string(pos) + " B" + std::to_string(pos) + " C" +
+                          std::to_string(pos) + " P" + std::to_string(pos) + " Q" + std::to_string(pos) + " K" +
+                          std::to_string(radium) + " I0" + "\n";
+                    break;
+                case 19:
+                    str = "N" + std::to_string(linenum) + " G3" + " X" + std::to_string(pos) + " Y" +
+                          std::to_string(pos) +
+                          " Z" + std::to_string(pos) + " A" + std::to_string(pos) + " B" + std::to_string(pos) + " C" +
+                          std::to_string(pos) + " P" + std::to_string(pos) + " Q" + std::to_string(pos) + " J" +
+                          std::to_string(radium) + " K0" + "\n";
+                    break;
+
+            }
+
+            break;
+        case 6:
+            switch (flat) {
+                case 17:
+                    str = "N" + std::to_string(linenum) + " G3" + " X" + std::to_string(pos) + " Y" +
+                          std::to_string(pos) +
+                          " Z" + std::to_string(pos) + " A" + std::to_string(pos) + " B" + std::to_string(pos) + " C" +
+                          std::to_string(pos) + " P" + std::to_string(pos) + " Q" + std::to_string(pos) + " R" +
+                          std::to_string(radium) + "\n";
+                    break;
+                case 18:
+                    str = "N" + std::to_string(linenum) + " G3" + " X" + std::to_string(pos) + " Y" +
+                          std::to_string(pos) +
+                          " Z" + std::to_string(pos) + " A" + std::to_string(pos) + " B" + std::to_string(pos) + " C" +
+                          std::to_string(pos) + " P" + std::to_string(pos) + " Q" + std::to_string(pos) + " R" +
+                          std::to_string(radium) + "\n";
+                    break;
+                case 19:
+                    str = "N" + std::to_string(linenum) + " G3" + " X" + std::to_string(pos) + " Y" +
+                          std::to_string(pos) +
+                          " Z" + std::to_string(pos) + " A" + std::to_string(pos) + " B" + std::to_string(pos) + " C" +
+                          std::to_string(pos) + " P" + std::to_string(pos) + " Q" + std::to_string(pos) + " R" +
+                          std::to_string(radium) + "\n";
+                    break;
+            }
+            break;
+        case 7:
+            switch (flat) {
+                case 17:
+                    str = "N" + std::to_string(linenum) + " G3" + " X" + std::to_string(pos) + " Y" +
+                          std::to_string(pos) +
+                          " Z" + std::to_string(pos) + " A" + std::to_string(pos) + " B" + std::to_string(pos) + " C" +
+                          std::to_string(pos) + " P" + std::to_string(pos) + " Q" + std::to_string(pos) + " R-" +
+                          std::to_string(radium) + "\n";
+                    break;
+                case 18:
+                    str = "N" + std::to_string(linenum) + " G3" + " X" + std::to_string(pos) + " Y" +
+                          std::to_string(pos) +
+                          " Z" + std::to_string(pos) + " A" + std::to_string(pos) + " B" + std::to_string(pos) + " C" +
+                          std::to_string(pos) + " P" + std::to_string(pos) + " Q" + std::to_string(pos) + " R-" +
+                          std::to_string(radium) + "\n";
+                    break;
+                case 19:
+                    str = "N" + std::to_string(linenum) + " G3" + " X" + std::to_string(pos) + " Y" +
+                          std::to_string(pos) +
+                          " Z" + std::to_string(pos) + " A" + std::to_string(pos) + " B" + std::to_string(pos) + " C" +
+                          std::to_string(pos) + " P" + std::to_string(pos) + " Q" + std::to_string(pos) + " R-" +
+                          std::to_string(radium) + "\n";
+                    break;
+            }
+            break;
+        case 8:
+            str = "N" + std::to_string(linenum) + " G4 K10\n";
+            break;
+        case 9:
+            str = "N" + std::to_string(linenum) + " G17\n";
+            flat = 17;
+            break;
+        case 10:
+            str = "N" + std::to_string(linenum) + " G18\n";
+            flat = 18;
+            break;
+        case 11:
+            str = "N" + std::to_string(linenum) + " G19\n";
+            flat = 19;
+            break;
+        case 12:
+            str = "N" + std::to_string(linenum) + " G50\n";
+            break;
+        case 13:
+            str = "N" + std::to_string(linenum) + " G51 D2\n";
+            break;
+        case 14:
+            str = "N" + std::to_string(linenum) + " G52\n";
+            break;
+        case 15:
+            str = "N" + std::to_string(linenum) + " G90\n";
+            relative = 90;
+            break;
+        case 16:
+            str = "N" + std::to_string(linenum) + " G91\n";
+            relative = 91;
+            break;
 
 
-	theG4code = "N0 G4 K3";
+    }
 
-	for (int i = 0; i < 17; i++)
-	{
-		for (int j = 0; j < 17; j++)
-		{
-			file << gcode[i].c_str() << "\n";
-			file << theG4code.c_str() << "\n";
-			file << gcode2[j].c_str() << "\n";
-			file << std::endl;
-		}
-		
-	}
+    return str;
+}
+
+
+void makeG4testFile(std::ofstream &file, std::ofstream &judgefile) {
+    int flat = 17;
+    int linenum = 0;
+    int pos_offset = 1;
+    int relative = 90;
+
+    for (int i = 0; i < 17; i++) {
+        for (int j = 0; j < 17; j++) {
+            file << makeGcode(i,linenum,pos_offset*10,flat,10,relative);
+            pos_offset++;
+            linenum++;
+            file << "N" << linenum << " G4 K3" << "\n";
+            linenum++;
+            file << makeGcode(j,linenum,pos_offset*10,flat,10,relative);
+            pos_offset++;
+            linenum++;
+            file << std::endl;
+        }
+
+    }
 
 
 }
